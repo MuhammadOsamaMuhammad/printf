2ad4ecff2372d26d25a8e302c7b84e714d7452b1
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2023-01-27 12:32:09 +0200

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int counter = 0;
+    
+    for (int i = 0; format[i] != '\0'; i++) 
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+            {
+                handle_char(args);
+                counter++;
+            }
+            else if (format[i] == 's')
+            {   counter += handle_string(args);
+            }
+            else if (format[i] == 'd' || format[i] == 'i') 
+            {   counter++;
+                handle_int(args);
+            }
+            else
+            {   counter++;
+                putchar('%');
+            }
+        } 
+        else 
+        {   counter++;
+            putchar(format[i]);
+        }
+    }
+    va_end(args);
+    return (counter);
+}
