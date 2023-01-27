cbf1601ab80af98be4c90da0e3128d975e323b2f
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-27 02:47:10 -0800

    tset

--- handle_string.c
+++ handle_string.c
@@ -1,12 +1,21 @@
 #include <stdarg.h>
 
+
+/**
+ * handle_string - handle strings for _printf
+ * @args: va list
+ * Return: number of chars of string
+ */
+
+
 int handle_string(va_list args)
 {
-char* s = va_arg(args, char*);
-int j;
-    for (j = 0; s[j] != '\0'; j++) 
-    {
-        _putchar(s[j]);
-    }
-    return (j+1);
+	char *s = va_arg(args, char *);
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		_putchar(s[j]);
+	}
+	return (j + 1);
 }
