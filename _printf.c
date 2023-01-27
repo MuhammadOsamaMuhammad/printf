366262f63646a292203ab7de09f8cd462359f016
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-27 03:19:28 -0800

    test

--- _printf.c
+++ _printf.c
@@ -1,39 +1,53 @@
 #include "main.h"
 #include <stdarg.h>
 
+
+
+/**
+ * _printf - similer to printf
+ * @format: pointer
+ * Return: int the number of printed chars
+ */
+
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
-    int counter = 0;
-    
-    for (int i = 0; format[i] != '\0'; i++) 
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                handle_char(args);
-                counter++;
-            }
-            else if (format[i] == 's')
-            {   counter += handle_string(args);
-            }
-            else if (format[i] == 'd' || format[i] == 'i') 
-            {   counter++;
-                handle_int(args);
-            }
-            else
-            {   counter++;
-                _putchar('%');
-            }
-        } 
-        else 
-        {   counter++;
-            _putchar(format[i]);
-        }
-    }
-    va_end(args);
-    return (counter);
+	va_list args;
+
+	va_start(args, format);
+	int counter = 0;
+
+	for (int i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				handle_char(args);
+				counter++;
+			}
+			else if (format[i] == 's')
+			{
+				counter += handle_string(args);
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+			{
+				counter++;
+				handle_int(args);
+			}
+			else
+			{
+				counter++;
+				_putchar('%');
+			}
+		}
+		else
+		{
+			counter++;
+			_putchar(format[i]);
+		}
+	}
+	va_end(args);
+	return (counter);
 }
