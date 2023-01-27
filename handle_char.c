03010ca26c8395fadca42365d5ae4b26dfb20391
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-27 03:05:24 -0800

    tst

--- handle_char.c
+++ handle_char.c
@@ -1,15 +1,17 @@
 #include <stdarg.h>
 
+
+
 /**
  * handle_char - function to handle chars for the _printf
  * @args: va_list
  * Return: void
  */
 
-
 void handle_char(va_list args)
 {
+
 	char c = (char)va_arg(args, int);
 
-	_putchar(c
+	_putchar(c);
 }
